6488a3e80c594bab19625acdeeb1290a0e062614
silkic <168343055+silkic@users.noreply.github.com>
2024-06-01 23:15:16 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insertion Sort/insertion-sort.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 new Solution().insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // This method inserts an element into its correct position within the sorted portion of the array
+    static void insert(int arr[], int i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+    // Function to sort the array using insertion sort algorithm
+    public void insertionSort(int arr[], int n) {
+        for (int i = 1; i < n; i++) {
+            insert(arr, i);
+        }
+    }
+}
