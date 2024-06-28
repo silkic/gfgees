3506e4c359f402a531612c36d404dd84e6908783
silkic <168343055+silkic@users.noreply.github.com>
2024-06-28 10:57:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        Arrays.sort(arr);
+        int l = arr[0];
+        int sl = -1;
+        for(int i = 0;i < n;i++){
+            if(l <= arr[i]){
+                l = arr[i];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] > sl && arr[i] != l){
+                sl = arr[i];
+            }
+        }
+        return sl;
+    }
+}
