6737c1f1a3ba7e8cc404b3010e2c74b0a2d09e7c
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-07-05 20:46:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java 1-d and 2-d Array/java-1d-and-2d-array.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Java program to demonstrate working of Scanner in Java
+import java.util.*;
+import java.io.*;
+class Abc
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int[] a[],b;
+            a= new int[n][n];
+            b= new int[n];
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    int p=sc.nextInt();
+                    a[i][j]=p;
+                }
+            }
+            int Max=0;
+            for(int i=0;i<n;i++)
+            {
+                int p=sc.nextInt();
+                b[i]=p;
+            }
+            Complete obj = new Complete();
+            ArrayList<Integer> ans;
+            ans = obj.array(a, b, n);
+            for(int i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Complete
+{
+    public static ArrayList<Integer> array(int a[][], int b[], int n)
+    {
+        // Complete the function
+        Arrays.sort(b);
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = 0;
+        for(int i = 0; i< n;i++){
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    sum += a[i][j];
+                }
+            }
+        }
+        int maxi = b[n-1];
+        list.add(sum);
+        list.add(maxi);
+        return list;
+    }
+}
