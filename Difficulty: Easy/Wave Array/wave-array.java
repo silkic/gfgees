3b89061728359b94a0d2beb00fda9fdd307d4cdf
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-09-01 22:21:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Wave Array/wave-array.java
+++ Difficulty: Easy/Wave Array/wave-array.java
@@ -52,12 +52,11 @@ class GFG {
 class Solution {
     public static void convertToWave(int n, int[] arr) {
         // code here
-        for(int i=0; i<n-1;i++){
-            if(i % 2 == 0){
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
+        for(int i = 0; i < n-1; i++){
+            if(i % 2 == 0)
+            {int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;}
         }
     }
 }
