a5f61cbf9e0411ccfe0d5c945986a4e8afce7a43
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-10-02 18:35:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+		
+		//taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total testcases
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    
+		    //taking count of stairs
+		    int m = sc.nextInt();
+		    
+		    //creating an object of class DynamicProgramming
+		    Solution obj = new Solution();
+		    
+		    //calling method countWays() of class
+		    //DynamicProgramming
+		    System.out.println(obj.countWays(m));
+		    
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to count number of ways to reach the nth stair.
+    static final int MOD = 1000000007;
+    int countWays(int n)
+    {
+        if(n <= 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        long dp[] = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])% MOD;
+        }
+        return (int)dp[n];
+    }
+}
+
