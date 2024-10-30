49c0a2e9b09be6137fa56779bcb2c09a9e80edb0
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-10-30 19:39:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Odd or Even/odd-or-even.java
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -22,7 +22,7 @@ class GFG {
 class Solution {
     static String oddEven(int n) {
         // code here
-        if(n % 2  == 0){
+        if(n % 2 == 0){
             return "even";
         }
         return "odd";
