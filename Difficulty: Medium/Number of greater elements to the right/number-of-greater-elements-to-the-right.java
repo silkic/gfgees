3e45aa67c82755802fa6ffbbb6372c56a342de8d
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2025-02-02 11:49:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for(int i=0;i<n;i++){
+        a[i]=sc.nextInt();
+      }
+      int q = sc.nextInt();
+      int ind[]=new int[q]; 
+      for(int i=0;i<q;i++) ind[i]=sc.nextInt();
+      Solution obj = new Solution();
+      int ans[] = obj.count_NGEs(n,a,q,ind);
+      for(int e : ans)
+      System.out.print(e+" ");
+      System.out.println();
+    
+System.out.println("~");
+}
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+  public static int[] count_NGEs(int N, int arr[], int queries, int indices[]) {
+    // code here
+    int[] res = new int[queries];
+    for(int q = 0; q < queries;q++){
+        int cnt = 0;
+        int ind = indices[q];
+        for(int i = ind+1;i < N;i++){
+            if(arr[ind] < arr[i]){
+                cnt++;
+            }
+        }
+        res[q] = cnt;
+    }
+    return res;
+   }
+} 
+     
