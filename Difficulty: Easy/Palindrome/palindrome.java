71c76d61dd4771964124ea0edb88139cd46b7b52
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2025-02-13 15:56:14 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // Number of test cases
+
+        Solution ob = new Solution();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            boolean ans = ob.isPalindrome(n);
+            System.out.println(ans ? "true" : "false");
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean isPalindrome(int n) {
+        // Code here
+        int orig = n;
+        int rev = 0;
+        while(n > 0){
+            rev = rev*10 +(n % 10);
+            n /= 10;
+        }
+        return orig == rev;
+    }
+}
