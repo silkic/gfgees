cf5c7c0ea73c888206e5d560dc7c3e494aaa984a
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2025-05-02 08:40:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findMaximum(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+}
